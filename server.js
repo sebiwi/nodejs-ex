2bdae147ec27726e552a0051fa58a203258e213d
Mathieu Herbert <math.herbert@gmail.com>
2016-11-18 15:04:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ initDb(function(err){
 });
 
 app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+console.log('Server running bis on http://%s:%s', ip, port);
 
 module.exports = app ;
